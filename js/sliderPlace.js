62d730be4a6072d820c3ddee16a6cb9b40acf4f2
letrungtin09 <letrungtin1312@gmail.com>
2024-04-03 22:00:50 +0700

    fix funtion slider for section places

--- js/sliderPlace.js
+++ js/sliderPlace.js
@@ -1,30 +1,59 @@
 let list = document.querySelector(".places__slider .places__list");
-let items = document.querySelectorAll(".places__slider .places__list .img1");
+// let items = document.querySelectorAll(".places__slider .places__list .img1");
 let next = document.getElementById("next");
 let prev = document.getElementById("prev");
 
 let active = 0;
-let lengthItems = items.length - 1;
+// let lengthItems = items.length - 1;
 
-next.onclick = function () {
+// next.onclick = function () {
+//   if (active + 1 > lengthItems) {
+//     active = 0;
+//   } else {
+//     active += 1;
+//   }
+//   reloadSlider(className);
+// };
+
+function nextImg(className) {
+  let items = document.querySelectorAll(
+    `.places__slider .places__list .${className}`
+  );
+  let lengthItems = items.length - 1;
   if (active + 1 > lengthItems) {
     active = 0;
   } else {
     active += 1;
   }
-  reloadSlider();
-};
+  reloadSlider(className);
+}
 
-prev.onclick = function () {
+function prevImg(className) {
+  let items = document.querySelectorAll(
+    `.places__slider .places__list .${className}`
+  );
+  let lengthItems = items.length - 1;
   if (active - 1 < 0) {
     active = lengthItems;
   } else {
     active -= 1;
   }
-  reloadSlider();
-};
+  reloadSlider(className);
+}
 
-function reloadSlider() {
+// prev.onclick = function () {
+//   if (active - 1 < 0) {
+//     active = lengthItems;
+//   } else {
+//     active -= 1;
+//   }
+//   reloadSlider();
+// };
+
+function reloadSlider(className) {
+  let items = document.querySelectorAll(
+    `.places__slider .places__list .${className}`
+  );
   let checkLeft = items[active].offsetLeft;
   list.style.left = -checkLeft + "px";
 }
